a8dcd596481b4610715130c2fb7293c5d4aef2de
OgechukwuCSC205 <yomacorp54@gmail.com>
2023-07-23 21:25:28 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import "./App.css";
+import Navbar from './components/Navbar/navbar';
+import Header from './components/Header/Header';
+import AboutUs from './components/AboutUs/AboutUs';
+import SpecialMenu from './components/SpecialMenu/specialMenu';
+import ContactUs from './components/ContactUs/contactUs';
+import Footer from './components/Footer/footer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Navbar/>
+        <Header/>
+        <AboutUs/>
+        <SpecialMenu/>
+        <ContactUs/>
+        <Footer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
